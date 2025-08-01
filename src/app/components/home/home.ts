45d7c9e33556c34877484ade768936b391f4e310
thefocusedvoyage <thefocusedvoyage@gmail.com>
2025-08-01 20:50:02 +0530

    lines change

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {NgxTypedJsModule} from 'ngx-typed-js';
 
 @Component({
   selector: 'app-home',
-  imports: [NgxTypedJsModule],
+  imports: [NgxTypedJsModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
@@ -12,8 +13,8 @@ export class Home {
 
 public typedStrings = [
     'Where every frame tells a story.',
-   'Memories fade, Photos don’t! Let me help you keep them alive.',
-   'The best part? It’s just a click away ↓'
+    'Wildlife, travel, and aerial photography expertly crafted.',
+    `The best part? It’s just a click away ↓`
   ];
 
 }
