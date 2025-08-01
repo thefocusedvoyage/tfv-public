0fed0e00a2e6e07c05b385ab8bd47bb92c416f26
thefocusedvoyage <thefocusedvoyage@gmail.com>
2025-08-01 19:32:23 +0530

    animation

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {NgxTypedJsModule} from 'ngx-typed-js';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgxTypedJsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
+
 export class Home {
+
+public typedStrings = [
+    'Where every frame tells a story.',
+   'Memories fade, Photos don’t! Let me help you keep them alive.',
+   'The best part? It’s just a click away ↓'
+  ];
 
 }
