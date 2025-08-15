df2330c90c6b2eb21bd23ec37f92f2052c5237d3
thefocusedvoyage <thefocusedvoyage@gmail.com>
2025-08-16 01:14:13 +0530

    svg addtions

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { gsap , ScrollTrigger} from '../../../vendor/gsap/gsap';
+gsap.registerPlugin(ScrollTrigger);
+let height: number;
+let speed = 100;
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss'
+  styleUrls: ['./contact.scss']
 })
-export class Contact {
+export class Contact implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+   
+  }
 
 }
